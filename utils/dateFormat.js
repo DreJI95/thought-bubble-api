a807c767703a30c80e10ac8f615041e60eae970e
Andre M <andre.ji.moseley@gmail.com>
2021-07-16 19:08:21 -0400

    updated date formatter

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -2,26 +2,22 @@
 module.exports = (
   timestamp,
 ) => {
+
+  let months = {
+    0: 'Jan',
+    1: 'Feb',
+    2: 'Mar',
+    3: 'Apr',
+    4: 'May',
+    5: 'Jun',
+    6: 'Jul',
+    7: 'Aug',
+    8: 'Sep',
+    9: 'Oct',
+    10: 'Nov',
+    11: 'Dec'
+  };
   
-  let months;
-
-  if (monthLength === 'short') {
-    months = {
-      0: 'Jan',
-      1: 'Feb',
-      2: 'Mar',
-      3: 'Apr',
-      4: 'May',
-      5: 'Jun',
-      6: 'Jul',
-      7: 'Aug',
-      8: 'Sep',
-      9: 'Oct',
-      10: 'Nov',
-      11: 'Dec'
-    };
-  }
-
   const dateObj = new Date(timestamp);
   const formattedMonth = months[dateObj.getMonth()];
   const dayOfMonth = dateObj.getDate();
